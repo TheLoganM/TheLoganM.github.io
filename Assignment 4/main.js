5b99e1492cd9519680cd257b397a580a8efc0e2d
Logan Morrow <logan.morrow03@outlook.com>
2025-03-31 14:42:28 -0400

    Commit 2 - Using a new .JS file to test to make sure my javascript is attaching properly. Also added replacement function/variables

--- Assignment 4/main.js
+++ Assignment 4/main.js
@@ -6,22 +6,24 @@ const story = document.querySelector('.story');
 
 
 function randomValueFromArray(array){
+  
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
 
-const StoryText = It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
-const insertx = [Willy the Goblin,Big Daddy,Father Christmas]
-const inserty = [the soup kitchen,Disneyland,the White House]
-const insertz = [spontaneously combusted,melted into a puddle on the sidewalk,turned into a slug and crawled away]
-
+const StoryText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+const insertx = ['Willy the Goblin','Big Daddy','Father Christmas']
+const inserty = ['the soup kitchen','Disneyland','the White House']
+const insertz = ['spontaneously combusted', 'melted into a puddle on the sidewalk','turned into a slug and crawled away']
 
 randomize.addEventListener('click', result);
 
 function result() {
+  let NewStory = StoryText
 
   if(customName.value !== '') {
     const name = customName.value;
+    name = name.replaceAll('Bob',customName)
 
   }
 
@@ -31,7 +33,7 @@ function result() {
 
   }
 
-  story.textContent = ;
+  story.textContent = NewStory
   story.style.visibility = 'visible';
   
   
@@ -41,3 +43,8 @@ const NewStory = StoryText
 const Xitem = randomValueFromArray(insertx)
 const Yitem = randomValueFromArray(inserty)
 const Zitem = randomValueFromArray(insertz)
+
+NewStory = NewStory.replaceAll ('insertx',Xitem)
+NewStory = NewStory.replaceAll ('inserty',Yitem)
+NewStory = NewStory.replaceAll ('insertz',Zitem)
+
