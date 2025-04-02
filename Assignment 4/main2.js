0791a384eb42c26aadde3b239b762c0c8ab2e0c7
Logan Morrow <logan.morrow03@outlook.com>
2025-04-02 01:23:13 -0400

    commit 4 - properly added the image selection function, just need to add darken btn

--- Assignment 4/main2.js
+++ Assignment 4/main2.js
@@ -4,20 +4,26 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-const Images = ['pic1','pic2','pic3','pic4','pic5']
 /* Declaring the array of image filenames */
-
+const images = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg']
 /* Declaring the alternative text for each image file */
+const alt = {
+    'pic1' : 'Image of eye (gross)',
+    'pic2' : 'Images of sand',
+    'pic3' : 'Image of purple and white flowers',
+    'pic4' : 'Image of Egyptian Hieroglyphics',
+    'pic5' : 'Image of Butterfly on leaf'
 
+}
 /* Looping through images */
-for (const image of Images){
-const newImage = document.createElement('img');
-newImage.setAttribute('src', `images/$pic1)`);
-newImage.setAttribute('alt',pic1);
-thumbBar.appendChild(newImage);
-newImage.addEventListener('click', e => {
-    displayedImage.src = e.target.src
-    displayedImage.alt = e.target.alt
-})
+for (const image of images){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt',[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+        displayedImage.src = e.target.src
+        displayedImage.alt = e.target.alt
+    })
 }
 /* Wiring up the Darken/Lighten button */
